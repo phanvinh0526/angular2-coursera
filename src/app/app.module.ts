7ef77df174b87bb307f52ddd755ee8a7b605694a
Vinh Phan <phanvinh0526@gmail.com>
2018-09-07 13:45:41 +1000

    add menu component using Flex layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,22 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar'; 
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatListModule } from '@angular/material/list';
 
 import 'hammerjs';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-
+    MatListModule,
+    
     // Import Modules
     BrowserAnimationsModule,
     MatToolbarModule,
---
+++ src/app/shared/dish.ts
@@ -0,0 +1,10 @@
+export class Dish{
+    // Create properties for Object Dish
+    name: string;
+    image: string;
+    category: string;
+    label: string;
+    price: string;
+    description: string;
+    
+}
